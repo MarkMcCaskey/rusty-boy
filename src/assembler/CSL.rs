a88927f929121f4c6c28c2550eaacaca74887ad5
MarkMcCaskey <rabidwaffle@gmail.com>
2017-02-07 06:35:25 -0500

    add 75% of instruction translation; further assembler

--- src/assembler/CSL.rs
+++ src/assembler/CSL.rs
@@ -24,9 +24,36 @@ pub struct Instruction {
 }
 
 pub fn unary_dispatch(inst: Instruction, v: Value) -> (u8, u8) {
+    // let z = (inst.prefix >> 6) & 3;
+    // match inst {
+    //
     unimplemented!();
+
 }
 
 pub fn binary_dispatch(inst: Instruction, v1: Value, v2: Value) -> (u8, u8, u8) {
     unimplemented!();
+}
+
+
+pub fn cpuReg_dispatch(reg: CpuRegister) -> u8 {
+    match reg {
+        CpuRegister::B => 0,
+        CpuRegister::C => 1,
+        CpuRegister::D => 2,
+        CpuRegister::E => 3,
+        CpuRegister::H => 4,
+        CpuRegister::L => 5,
+        CpuRegister::HL => 6,
+        CpuRegister::A => 7,
+        _ => unreachable!(),
+    }
+}
+
+/// Returns in Big Endian
+pub fn lookup_prefix(n: u8, reg: CpuRegister) -> (u8, u8) {
+    let z = cpuReg_dispatch(reg);
+    let value = (n << 4) | z;
+
+    (0xCB, value)
 }
