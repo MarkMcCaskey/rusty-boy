bf4ce3ca81a9c8452c9a8e0251806d8308ea3372
SpawnedArtifact <spwndrtfct@gmail.com>
2017-03-02 00:45:31 +0300

    vidram: Add incorrect tile drawing

--- src/io/constants.rs
+++ src/io/constants.rs
@@ -63,6 +63,10 @@ pub const TILE_PALETTE: [Color; 4] = [Color::RGB(4, 5, 7),
                                       Color::RGB(235, 135, 140),
                                       Color::RGB(156, 146, 244),
                                       Color::RGB(252, 250, 175)];
+pub const OBJECT_PALETTE: [Color; 4] = [Color::RGB(184, 248, 24),
+                                        Color::RGB(174, 124, 9),
+                                        Color::RGB(248, 184, 0),
+                                        Color::RGB(248, 240, 240)];
 
 pub const SCREEN_BUFFER_SIZE_X: u32 = 256;
 pub const SCREEN_BUFFER_SIZE_Y: u32 = 256;
@@ -73,5 +77,9 @@ pub const SCREEN_BUFFER_TILES_Y: u32 = 32;
 pub const GB_SCREEN_WIDTH: u8 = 160;
 pub const GB_SCREEN_HEIGHT: u8 = 144;
 
-
 pub const NICER_COLOR: sdl2::pixels::Color = sdl2::pixels::Color::RGBA(139, 41, 2, 255);
+
+pub const OBJECT_ATTRIBUTE_START: u16 = 0xFE00;
+pub const OBJECT_ATTRIBUTE_END: u16 = 0xFE9F;
+pub const OBJECT_ATTRIBUTE_BLOCK_SIZE: u16 = 4;
+
--- src/io/vidram.rs
+++ src/io/vidram.rs
@@ -44,6 +44,7 @@ impl Drawable for VidRamBGDisplay {
                                tile_map_offset,
                                tile_patterns_offset,
                                0);
+        draw_objects(renderer, cpu, 0, 0);
     }
     
     fn click(&mut self, _: sdl2::mouse::MouseButton, _: Point, _: &mut Cpu) {
@@ -121,6 +122,46 @@ pub fn draw_tile(renderer: &mut sdl2::render::Renderer,
     }
 }
 
+// TODO cache tiles into texture and use blending options (?) to make
+// desired color "trasnparent".
+/// Draw single tile at given screen position without drawing "0" color.
+pub fn draw_tile_transparent(renderer: &mut sdl2::render::Renderer,
+                             gameboy: &Cpu,
+                             mem_offset: u16,
+                             tile_idx: u16, // technically when used by GB it's only 8bit
+                             screen_offset_x: i32,
+                             screen_offset_y: i32) {
+
+    #[inline]
+    fn get_bit(n: u8, offset: u8) -> u8 {
+        (n >> (7 - offset)) & 1u8
+    }
+    
+    for px in 0..TILE_SIZE_PX {
+        for py in 0..TILE_SIZE_PX {
+            let col_byte_off = py * 2;
+            let offset = mem_offset + (tile_idx * TILE_SIZE_BYTES);
+            let col_byte1_v = gameboy.mem[(offset + col_byte_off) as usize];
+            let col_byte2_v = gameboy.mem[(offset + col_byte_off + 1) as usize];
+            let col_bit_1 = get_bit(col_byte1_v, px as u8);
+            let col_bit_2 = get_bit(col_byte2_v, px as u8);
+            let px_color = (col_bit_2 << 1) | col_bit_1;
+
+            // let d = 255/4;
+            // let px_val = px_color*d;
+            // renderer.set_draw_color(Color::RGB(px_val, px_val, px_val));
+
+            if true || px_color != 0 {
+                let px_pal_col = OBJECT_PALETTE[px_color as usize];
+                renderer.set_draw_color(px_pal_col);
+                
+                let point = Point::new((screen_offset_x as u8).wrapping_add(px as u8) as i32,
+                                       (screen_offset_y as u8).wrapping_add(py as u8) as i32);
+                renderer.draw_point(point).unwrap();
+            }
+        }
+    }
+}
 
 /// This is the dumbest and straightforward code for displaying Tile
 /// Patterns. It displays both background and sprite "tiles" as they
@@ -226,4 +267,60 @@ fn draw_screen_border(renderer: &mut sdl2::render::Renderer,
         }
     }
     renderer.set_clip_rect(None);
+}
+
+
+/// Draw "sprites" (something gameboy calls "Objects").
+pub fn draw_objects(renderer: &mut sdl2::render::Renderer,
+                    gameboy: &Cpu,
+                    screen_offset_x: i32,
+                    screen_offset_y: i32) {
+
+    // TODO sprites should use palletes
+    // TODO sprites can have transparent color
+    // TODO sprites are sorted by x/y/idx
+    
+    for obj_idx in 0..40+1 {
+        let offset = OBJECT_ATTRIBUTE_START + obj_idx * OBJECT_ATTRIBUTE_BLOCK_SIZE;
+        let sprite_y: u8 = gameboy.mem[offset as usize];
+        let sprite_x: u8 = gameboy.mem[offset as usize + 1];
+        let tile_index: i8 = gameboy.mem[offset as usize + 2] as i8;
+        // TODO implement flag handling (priority, flips...)
+        let flags: u8 = gameboy.mem[offset as usize + 3];
+        if sprite_x == 0 && sprite_y == 0 {
+            // sprite is "hidden"
+            continue
+        }
+        let screen_x = sprite_x.wrapping_sub(8);
+        let screen_y = sprite_y.wrapping_sub(16);
+        
+        if gameboy.lcdc_sprite_size() {
+            // "Tall 8x16 sprites" mode
+            // FIXME Not sure this is how 8x16 sprites work
+            let tile_16 = tile_index & !1;
+            draw_tile_transparent(renderer,
+                                  gameboy,
+                                  TILE_PATTERN_TABLE_2_START,
+                                  add_u16_i8(128u16, tile_16), // index is signed 8bit
+                                  screen_offset_x + screen_x as i32,
+                                  screen_offset_y + screen_y as i32);
+            draw_tile_transparent(renderer,
+                                  gameboy,
+                                  TILE_PATTERN_TABLE_2_START,
+                                  add_u16_i8(128u16, (tile_16 + 1)), // index is signed 8bit
+                                  screen_offset_x + screen_x as i32,
+                                  screen_offset_y + screen_y as i32);
+        } else {
+            // 8x8 sprites mode
+            draw_tile_transparent(renderer,
+                                  gameboy,
+                                  TILE_PATTERN_TABLE_2_START,
+                                  add_u16_i8(128u16, (tile_index as i8)), // index is signed 8bit
+                                  screen_offset_x + screen_x as i32,
+                                  screen_offset_y + screen_y as i32);
+
+        }
+        
+    }
+       
 }
