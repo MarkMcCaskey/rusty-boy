0d3b6391da6873ccca5b0a1d93a175c37c0c3038
Mark McCaskey <maoeurk@gmail.com>
2023-07-11 10:15:23 +0800

    Uncomment channel 1 and 2

--- src/io/dr_sdl2.rs
+++ src/io/dr_sdl2.rs
@@ -405,7 +405,6 @@ impl Renderer for Sdl2Renderer {
         sound_system.channel1.enabled = gb.get_sound1();
         sound_system.channel2.enabled = gb.get_sound2();
         sound_system.channel3.enabled = gb.get_sound3();
-        /*
         if gb.get_sound1() {
             sound_system.channel1.volume = gb.channel1_envelope_volume() as f32 / 15.0;
             sound_system.channel1.wave_duty = gb.channel1_wave_pattern_duty();
@@ -419,7 +418,6 @@ impl Renderer for Sdl2Renderer {
             let channel2_freq = 4194304.0 / (4.0 * 8.0 * (2048.0 - gb.channel2_frequency() as f32));
             sound_system.channel2.phase_inc = channel2_freq / sound_system.out_freq;
         }
-        */
 
         if gb.get_sound3() {
             let channel3_freq = 4194304.0 / (4.0 * 8.0 * (2048.0 - gb.channel3_frequency() as f32));
